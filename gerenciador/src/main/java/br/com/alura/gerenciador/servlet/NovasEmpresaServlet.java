6f169084e9bcfbb0b263690ca6ce8baf4e3fde03
Carlos Henrique <ichfs9108@gmail.com>
2022-06-23 02:02:43 -0300

    definindo modelo

---
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/NovasEmpresaServlet.java
@@ -0,0 +1,35 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class NovasEmpresaServlet
+ */
+@WebServlet("/novasEmpresa")
+public class NovasEmpresaServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("Cadastrando nova empresa");
+
+		String nomeEmpresa = request.getParameter("nome");
+		Empresa empresa = new Empresa();
+		empresa.setNome(nomeEmpresa);
+		
+		Banco banco = new Banco();
+		banco.adiciona(empresa);
+		
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>Empresa " + nomeEmpresa + " cadastrada com sucesso!</body></html>");
+	}
+
+}
